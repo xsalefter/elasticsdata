5cb7600f53ded08c59f11aac63b0c3cce748eb2f
xsalefter <xsalefter@gmail.com>
2017-06-04 06:57:49 +0700

    change configuration for "official community" spring data elasticsearch

--- src/main/java/com/xsalefter/elasticsdata/Application.java
+++ src/main/java/com/xsalefter/elasticsdata/Application.java
@@ -2,10 +2,8 @@ package com.xsalefter.elasticsdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 
-@SpringBootApplication(exclude = {ElasticsearchAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String...args) {
